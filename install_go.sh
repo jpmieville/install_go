96bbf1de0e92ccb34b76020c00324b9fa4274c63
Jean-Paul Mieville <jpmieville@gmail.com>
2022-05-29 11:29:49 +0200

    firs version

---
+++ install_go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION="1.18.2" # go version
+ARCH="amd64"     # go archicture 
+wget -L "https://golang.org/dl/go${VERSION}.linux-${ARCH}.tar.gz"
+if [[ -d /usr/local ]]
+then
+    rm -rf /usr/local/go
+fi
+tar -C /usr/local -xzf go${VERSION}.linux-amd${ARCH}.tar.gz
