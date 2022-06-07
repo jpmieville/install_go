3ca53df27367ff1810b73018e125ce03a966a3f1
Jean-Paul Miéville <jpmieville@gmail.com>
2022-06-07 14:19:19 +0200

    correct a bug
    To be tested

--- install_go.sh
+++ install_go.sh
@@ -2,8 +2,9 @@
 VERSION="1.18.2" # go version
 ARCH="amd64"     # go archicture 
 wget -L "https://golang.org/dl/go${VERSION}.linux-${ARCH}.tar.gz"
-if [[ -d /usr/local ]]
+if [[ -d /usr/local/go ]]
 then
+    echo "Remove the old version of go"
     rm -rf /usr/local/go
 fi
 tar -C /usr/local -xzf go${VERSION}.linux-${ARCH}.tar.gz
